3abccc27bf35adc4e88d50cefbe0d980bf73a82c
Dolan Murvihill <dmurvihill@gmail.com>
2015-04-04 20:16:03 -0400

    add options for controlling instance types and zones

--- src/instances.py
+++ src/instances.py
@@ -2,20 +2,22 @@ import boto
 import boto.ec2
 
 ami_ubuntu = 'ami-29ebb519'
-ec2 = boto.ec2.connect_to_region('us-west-2')
 
-def start_instance(type='t2.micro', placement=None):
+def start_instance(
+        ec2=boto.ec2.connect_to_region('us-west-2'),
+        type='t2.micro', placement=None):
     ''' Start an EC2 instance using the default settings that we care about '''
     return start_instances(1,type,placement)[0]
 
-def start_instances(n, type='t2.micro', zone=None):
+def start_instances(n, ec2=boto.ec2.connect_to_region('us-west-2'),
+        type='t2.micro', zone=None):
     ''' Start multiple EC2 instances using the default settings that we care
     about '''
     return ec2.run_instances(ami_ubuntu, instance_type=type,
                              min_count=n, max_count=n,
                              placement=zone).instances
 
-def terminate_instance(id):
+def terminate_instance(id, ec2=boto.ec2.connect_to_region('us-west-2')):
     ''' Wrapper for the EC2 terminate instance command '''
     return ec2.terminate_instances(id)
 
